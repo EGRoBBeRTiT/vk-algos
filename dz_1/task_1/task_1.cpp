f4b0f4e800991ea0911d818117cfa127c1e2c448
Егор Титов <ea.titov@mail.ru>
2023-10-23 13:35:36 +0300

    task_1 done

---
+++ dz_1/task_1/task_1.cpp
@@ -0,0 +1,87 @@
+#include <assert.h>
+#include <iostream>
+#include <sstream>
+
+// Инвертируйте значение бита в числе N по его номеру K.
+// Формат входных данных. Число N, номер бита K.
+// Формат выходных данных. Число с инвертированным битом в десятичном виде.
+
+void run(std::istream& in, std::ostream& out);
+unsigned int inverse_bit(const unsigned int& N, const unsigned int& K);
+void test();
+
+int main(void) {
+    // test();
+    run(std::cin, std::cout);
+
+    return 0;
+}
+
+// -------------------------------------------
+
+unsigned int inverse_bit(const unsigned int& N, const unsigned int& K) {
+    return N ^ (1 << K);
+}
+
+void run(std::istream& in, std::ostream& out) {
+    unsigned int N = 0;
+    unsigned int K = 0;
+
+    in >> N >> K;
+
+    out << inverse_bit(N, K);
+}
+
+void test() {
+    {
+        std::stringstream in;
+        std::stringstream out;
+
+        in << "25 1";
+
+        run(in, out);
+
+        assert(out.str() == "27");
+
+        std::cout << "OK" << std::endl;
+    }
+
+    {
+        std::stringstream in;
+        std::stringstream out;
+
+        in << "25 4";
+
+        run(in, out);
+
+        assert(out.str() == "9");
+
+        std::cout << "OK" << std::endl;
+    }
+
+    {
+        std::stringstream in;
+        std::stringstream out;
+
+        in << "25 0";
+
+        run(in, out);
+
+        assert(out.str() == "24");
+
+        std::cout << "OK" << std::endl;
+    }
+
+    {
+        std::stringstream in;
+        std::stringstream out;
+
+        in << "1 0";
+
+        run(in, out);
+
+        assert(out.str() == "0");
+
+        std::cout << "OK" << std::endl;
+    }
+}
